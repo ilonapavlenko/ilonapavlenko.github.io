c322109d1fa6c210e9980aae4376895e5e993e9d
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2022-10-12 18:46:30 +0300

    Create js5.js

---
+++ js5.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", function (event) 
+{
+    console.log("DOM fully loaded and parsed");
+    function calculate() 
+    {
+        var count = document.getElementById("count").value;
+        var price = document.getElementById("price").value;
+        var res = document.getElementById("result");
+
+        var err=/^(?!(0))\d+$/;
+        if(count.match(err)===null || price.match(err)===null)
+        {
+            alert("Неккоректный ввод");
+            res.innerHTML="Ошибка";
+        }
+        else
+        {
+            res.innerHTML = price*count;
+        }
+    
+    }
+    var b = document.getElementById("button1");
+    b.addEventListener("click", calculate);
+}
+);
