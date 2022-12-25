c7453e45355e2bb15c86a5ac121bef26bfcd1f8c
GlebPivanov <112856583+GlebPivanov@users.noreply.github.com>
2022-12-25 18:38:11 +0300

    Update project.js

--- project.js
+++ project.js
@@ -18,4 +18,26 @@ $(document).ready(function(){
         $(this).removeClass('scaled');
         $('.plans-card-business').removeClass('business-scale');
     });
+    
+    // СЛАЙДЕР ДЛЯ ОТЗЫВОВ
+
+    $('.reviews-slider').on(`init reInit`, function (event, slick) {
+        $('.slick-slide-num-current').text('0' + 1 + ' '); 
+    })
+    $('.reviews-slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+        $('.slick-slide-num-current').text('0' + (currentSlide + 1) + ' '); 
+    })
+    $('.reviews-slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        easing: 'ease',
+        appendArrows: '.slick-arrows',
+        prevArrow: '<span class="slick-prev"><</span>',
+        nextArrow: '<span class="slick-next">></span>'
+
+    });
   });
