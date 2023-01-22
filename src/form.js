4d346f8ffd348ad3f7c8ca46473cdd79363cd67c
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-01-22 15:22:15 +0300

    Update form.js

--- src/form.js
+++ src/form.js
@@ -18,7 +18,7 @@ class LikeButton extends React.Component {
             onClick = {e => e.stopPropagation()}>
             <form action="https://formcarry.com/s/i9EtQjA3zV1" method="POST" acceptCharset="UTF-8" id="form">
               <div>
-                <input className = "webform__form-elem"
+                <input className = "form-form-elem"
                   id = "form-name"
                   type = "text"
                   name = "name"
@@ -26,7 +26,7 @@ class LikeButton extends React.Component {
                   placeholder="Ваше имя" />
               </div>
               <div>
-                <input className = "webform__form-elem"
+                <input className = "form-form-elem"
                   id = "form-phone"
                   type = "tel"
                   name = "phone"
@@ -34,7 +34,7 @@ class LikeButton extends React.Component {
                   placeholder="Телефон" />
               </div>
               <div>
-                <input className = "webform__form-elem"
+                <input className = "form-form-elem"
                   id = "form-email"
                   type = "email"
                   name = "email"
@@ -43,22 +43,22 @@ class LikeButton extends React.Component {
               </div>
               <div>
                 <input id = "form-comment"
-                  className = "webform__form-elem"
+                  className = "form-form-elem"
                   type = "text"
                   placeholder = "Ваш комментарий" />
               </div>
-              <div className = "form__checkbox" >
-                <input className = "checkbox__input"
+              <div className = "form-checkbox" >
+                <input className = "checkbox-input"
                   type = "checkbox"
                   id = "userAgreement" />
                 <label
-                  className="checkbox__label"
+                  className="checkbox-label"
                   type = "text"
                   htmlFor="userAgreement" > Отправляя заявку я даю согласие на <a> обработку своих персональных данных </a>
                 </label>
               </div>
               <div>
-                <input className = "webform__form-btn"
+                <input className = "form-form-btn"
                   type="submit"
                   value={"ОСТАВИТЬ ЗАЯВКУ!"}/>
               </div>
@@ -71,7 +71,7 @@ class LikeButton extends React.Component {
     return (
       <div>
         <input onClick={() =>this.setState({ liked: true })}
-          className = "plans__btn"
+          className = "plans-btn"
           type = "button"
           value = "ОСТАВИТЬ ЗАЯВКУ!!!" />
       </div>
